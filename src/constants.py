3a722273c2bd5a0236e83870d253e38e2c24c489
agustin <agustin.piqueres@gmail.com>
2021-09-14 21:18:04 +0200

    feat: add paths for distributions and reducto reports

--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,8 @@ import pathlib
 DATA_FOLDER: pathlib.Path = pathlib.Path(".").resolve() / 'data'
 EXTERNAL: pathlib.Path = DATA_FOLDER / 'external'
 INTERIM: pathlib.Path = DATA_FOLDER / 'interim'
+DISTRIBUTIONS: pathlib.Path = INTERIM / 'distributions'
+REDUCTO_REPORTS: pathlib.Path = INTERIM / 'reducto_reports'
 PROCESSED: pathlib.Path = DATA_FOLDER / 'processed'
 RAW: pathlib.Path = DATA_FOLDER / 'raw'
 
