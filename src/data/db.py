59aa2168496575eeb5e5bb44cfb9a13c73c65ed6
agustin <agustin.piqueres@gmail.com>
2021-09-16 00:35:29 +0200

    feat: function added to insert reducto reports

--- src/data/db.py
+++ src/data/db.py
@@ -2,6 +2,7 @@
 
 """
 
+from typing import Dict
 import pathlib
 
 from tinydb import TinyDB, Query
@@ -10,27 +11,47 @@ DB_PATH = pathlib.Path('.').resolve() / 'data' / 'processed' / 'db.json'
 
 db = TinyDB(DB_PATH, sort_keys=True, indent=4)
 
-db.insert({'type': 'peach', 'count': 3})
-db.insert({'type': 'apple', 'count': 2})
+reducto_reports_table = db.table('reducto_reports')
 
 
-TABLE_DOWNLOADS = 'downloads'
-Fruit = Query()
-db.search(Fruit.type == 'peach')
+# db.insert({'type': 'peach', 'count': 3})
+# db.insert({'type': 'apple', 'count': 2})
+#
+#
+# TABLE_DOWNLOADS = 'downloads'
+# Fruit = Query()
+# db.search(Fruit.type == 'peach')
 
-downloads = db.table(TABLE_DOWNLOADS)
+# downloads = db.table(TABLE_DOWNLOADS)
 # name, path = download.get_package('click', download.PACKAGES_DIRECTORY)  #
 # downloads.insert({'package': result[0]})
-package_info = {
-    'pypi_package': 'PATH_TO_ORIGINAL_PACKAGE',
-    'reducto_target': 'FIND_PATH_TO_SOURCE',  # Create helper function
-    'version': 'PATH_TO_ORIGINAL_PACKAGE',
-    'processed': 'BOOL',  # Whether the package is already processed or not. Remove when done
-    'reducto_stats': 'REDUCTO_EXECUTED_ON_REDUCTO_TARGET',
-    'libraries': 'OBTAINED_FROM_PYBRARIES'
-}
+# package_info = {
+#     'pypi_package': 'PATH_TO_ORIGINAL_PACKAGE',
+#     'reducto_target': 'FIND_PATH_TO_SOURCE',  # Create helper function
+#     'version': 'PATH_TO_ORIGINAL_PACKAGE',
+#     'processed': 'BOOL',  # Whether the package is already processed or not. Remove when done
+#     'reducto_stats': 'REDUCTO_EXECUTED_ON_REDUCTO_TARGET',
+#     'libraries': 'OBTAINED_FROM_PYBRARIES'
+# }
 
-example = {
-    'package_name_1': 'package_info_1',  # Obtained
-    'package_name_2': 'package_info_2',
-}
+# example = {
+#     'package_name_1': 'package_info_1',  # Obtained
+#     'package_name_2': 'package_info_2',
+# }
+
+
+def insert_reducto_reports(report: Dict) -> None:
+    """Insert the reducto report of the package to db.json.
+
+    Parameters
+    ----------
+    report : Dict
+
+    Examples
+    --------
+    >>> import src.data.reducto_process as rp
+    >>> report = rp.read_reducto_report('click')
+    >>> insert_reducto_reports(report)
+    """
+    reducto_reports_table.insert(report)
+
