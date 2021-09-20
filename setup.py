be3d7a2416aebfe3e45ed30c8b8c651e56d164c1
agustin <agustin.piqueres@gmail.com>
2021-09-20 22:38:43 +0200

    fix: description error on apostrophes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Python's source code statistics from top PyPI packages',
+    description="Python's source code statistics from top PyPI packages",
     author='Agustín',
     license='MIT',
 )
