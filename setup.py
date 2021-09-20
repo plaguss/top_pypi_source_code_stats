2e65bec4d8ef8381514fcc2aa8e7f148b26915ab
agustin <agustin.piqueres@gmail.com>
2021-09-20 23:22:36 +0200

    feat: add reducto_reports as command

--- setup.py
+++ setup.py
@@ -7,4 +7,8 @@ setup(
     description="Python's source code statistics from top PyPI packages",
     author='Agustín',
     license='MIT',
+    entry_points='''
+       [console_scripts]
+       reducto_reports=src.data.make_dataset:reducto_reports
+   ''',
 )
