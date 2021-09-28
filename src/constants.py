e1663bf2cb713e4013d2c87bbf78ed13df7e40b9
agustin <agustin.piqueres@gmail.com>
2021-09-28 23:50:23 +0200

    fix: relative paths corrected to load from notebooks

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,8 @@
 import pathlib
 
 # Data folders paths
-DATA_FOLDER: pathlib.Path = pathlib.Path(".").resolve() / 'data'
+# DATA_FOLDER: pathlib.Path = pathlib.Path(".").resolve() / 'data'
+DATA_FOLDER: pathlib.Path = pathlib.Path(__file__).resolve().parent.parent / 'data'
 EXTERNAL: pathlib.Path = DATA_FOLDER / 'external'
 INTERIM: pathlib.Path = DATA_FOLDER / 'interim'
 DISTRIBUTIONS: pathlib.Path = INTERIM / 'distributions'
@@ -13,3 +14,6 @@ RAW: pathlib.Path = DATA_FOLDER / 'raw'
 
 # Database path
 DB_PATH: pathlib.Path = PROCESSED / 'db.json'
+
+# Reducto table
+REDUCTO_TABLE = pathlib.Path = PROCESSED / 'reducto_reports.csv'
