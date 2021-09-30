44d5301dd1527e226a57e7ddf29a490b49382f05
agustin <agustin.piqueres@gmail.com>
2021-09-30 23:05:00 +0200

    feat: sample of kmeans

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,6 +2,8 @@
 """
 
 import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
 import src.constants as cte
 
@@ -20,8 +22,8 @@ def get_reducto_reports_table() -> pd.DataFrame:
 
 
 def get_reducto_reports_table_no_outliers() -> pd.DataFrame:
-    """Remove from the initial table the packages with more than 1e6 lines of code,
-    and those with average function length smaller than 200
+    """Removes every package outside of 6 standard deviations from the columns
+    lines, average_function_length, number_of_functions and source_files.
 
     Returns
     -------
@@ -29,8 +31,15 @@ def get_reducto_reports_table_no_outliers() -> pd.DataFrame:
     """
     table = get_reducto_reports_table()
     # Packages with less than a million lines of code
-    no_outliers = table[table['lines'] < 1e6]
-    no_outliers = no_outliers[no_outliers['average_function_length'] < 200]
+    n_std = 6
+    idx1 = table['lines'] < table['lines'].std() * n_std
+    idx2 = table['average_function_length'] < table['average_function_length'].std() * n_std
+    idx3 = table['number_of_functions'] < table['number_of_functions'].std() * n_std
+    idx4 = table['source_files'] < table['source_files'].std() * n_std
+
+    # Remove rows with more than 6 std in any column
+    no_outliers = table[idx1 * idx2 * idx3 * idx4]
+
     return no_outliers
 
 
@@ -51,3 +60,43 @@ def get_reducto_reports_relative() -> pd.DataFrame:
         )
     return no_outliers
 
+
+def get_pc(data: pd.DataFrame, standardize: bool = False) -> pd.DataFrame:
+    # data = get_reducto_reports_table_no_outliers()
+    # pca = PCA(n_components=2).fit(data)
+    # pca.explained_variance_ratio_
+    if standardize:
+        data = (data - data.mean()) / data.std()
+    reduced_data = PCA(n_components=2).fit_transform(data)
+    return pd.DataFrame(reduced_data, columns=['PC1', 'PC2'])
+
+
+def number_of_clusters(data: pd.DataFrame):
+    # import matplotlib.pyplot as plt
+
+    from sklearn.metrics import silhouette_samples, silhouette_score
+
+    range_n_clusters = [2, 3, 4, 5, 6, 7, 8]
+
+    for n_clusters in range_n_clusters:
+        # fig, (ax1, ax2) = plt.subplots(1, 2)
+        # fig.set_size_inches(18, 7)
+        # ax1.set_xlim([-0.1, 1])
+        # The (n_clusters+1)*10 is for inserting blank space between silhouette
+        # plots of individual clusters, to demarcate them clearly.
+        # ax1.set_ylim([0, len(data) + (n_clusters + 1) * 10])
+
+        clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+        cluster_labels = clusterer.fit_predict(data)
+
+        silhouette_avg = silhouette_score(data, cluster_labels)
+        print("For n_clusters =", n_clusters,
+              "The average silhouette_score is :", silhouette_avg)
+
+        # Compute the silhouette scores for each sample
+        sample_silhouette_values = silhouette_samples(data, cluster_labels)
+
+
+def kmeans_clustering(data: pd.DataFrame, clusters: int = 2):
+    k_means = KMeans(n_clusters=clusters, n_init=10)
+    k_means.fit(data)
