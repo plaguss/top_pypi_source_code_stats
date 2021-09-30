83643cd8e568288c4e00f24edecd21988a67ed66
agustin <agustin.piqueres@gmail.com>
2021-09-30 15:55:53 +0200

    feat: added tables without outliers for easy plotting

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -16,4 +16,38 @@ def get_reducto_reports_table() -> pd.DataFrame:
     -------
     data : pd.DataFrame
     """
-    return pd.read_csv(cte.PROCESSED / 'reducto_reports.csv', index_col=0)
+    return pd.read_csv(cte.REDUCTO_TABLE, index_col=0)
+
+
+def get_reducto_reports_table_no_outliers() -> pd.DataFrame:
+    """Remove from the initial table the packages with more than 1e6 lines of code,
+    and those with average function length smaller than 200
+
+    Returns
+    -------
+    data : pd.DataFrame
+    """
+    table = get_reducto_reports_table()
+    # Packages with less than a million lines of code
+    no_outliers = table[table['lines'] < 1e6]
+    no_outliers = no_outliers[no_outliers['average_function_length'] < 200]
+    return no_outliers
+
+
+def get_reducto_reports_relative() -> pd.DataFrame:
+    """From the table without outliers, returns the number of lines as a percentage of
+    the total number of lines (for the variables
+
+    Returns
+    -------
+
+    """
+    no_outliers = get_reducto_reports_table_no_outliers()
+    total_lines = no_outliers['lines']
+    no_outliers[['source_lines', 'blank_lines', 'docstring_lines', 'comment_lines']] = \
+        no_outliers[
+            ['source_lines', 'blank_lines', 'docstring_lines', 'comment_lines']].divide(
+            total_lines, axis=0
+        )
+    return no_outliers
+
